4e1d72b5c67b895159ef17d56c69319fce13a173
Giovanni Karra <karragiovanni@gmail.com>
2024-10-31 01:07:11 +0100

    deployment script modularity

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,39 @@
+#!/bin/env sh
+
 rm -rf build
 
 mkdir -p build
 mkdir -p build/backend
-mkdir -p build/backend/database
+mkdir -p build/database
 mkdir -p build/frontend
 
-cd backend
+cd backend || exit
 cargo build --release --target=aarch64-unknown-linux-gnu
 cp target/aarch64-unknown-linux-gnu/release/backend ../build/backend/nnn-server
-cp -r database/ ../build/backend/
 cp .env ../build/backend/
 cd ..
 
-cd frontend
+cp -r database/ build/
+
+cd frontend || exit
 npx vite build
 cp -r dist ../build/frontend/dist
 cd ..
 
-cd build/backend/database
+cd build/database || exit
 sh dbreset.sh
-cd ../../..
+cd ..
 
-echo "pkill server" | ssh 192.168.1.29
+TO_COPY=frontend
+if [ "$1" -ge 1 ]; then
+	echo "pkill server" | ssh 192.168.1.29
+	TO_COPY="$TO_COPY backend"
+fi
+if [ "$2" -ge 1 ]; then
+	TO_COPY="$TO_COPY database"
+fi
+scp -r $TO_COPY 192.168.1.29:NNN-With-Friends
 
-cd build
-scp -r * 192.168.1.29:NNN-With-Friends
-
-echo "cd NNN-With-Friends/backend; ./nnn-server" | ssh 192.168.1.29
+if [ "$1" -ge 1 ]; then
+	echo "cd NNN-With-Friends/backend; ./nnn-server" | ssh 192.168.1.29
+fi
